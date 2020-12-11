0105fb29bcc69c91c7947ccba2e671ac58ea25ff
Lucas Aragão <lucas.magno@dcx.ufpb.br>
2020-12-11 00:33:47 -0300

    Projetoo final

---
+++ crdb/src/main/java/com/ApiCrdb/crdb/CrdbApplication.java
@@ -0,0 +1,13 @@
+package com.ApiCrdb.crdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrdbApplication.class, args);
+	}
+
+}
---
+++ crdb/src/test/java/com/ApiCrdb/crdb/CrdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ApiCrdb.crdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
